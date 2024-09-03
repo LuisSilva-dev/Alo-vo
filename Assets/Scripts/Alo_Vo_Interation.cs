4830c00771b035d053ab180c907dce93b12159a7
LuisSilva-dev <lcd.silva@campus.fct.unl.pt>
2024-09-03 16:46:37 +0100

    mudança de component para Image

---
+++ Assets/Scripts/Alo_Vo_Interation.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Alo_Vo_Interation : MonoBehaviour
+{
+    private string URL = "http://ip.jsontest.com/?callback=showMyIP";
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("hello");
+        StartCoroutine(GetAnswer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator GetAnswer()
+    {
+        Debug.Log("Oi in answer!");
+        using(UnityWebRequest request = UnityWebRequest.Get(URL))
+        {
+            yield return request.SendWebRequest();
+            
+            if(request.result == UnityWebRequest.Result.ConnectionError)
+                Debug.LogError(request.error);
+            else
+            {
+                string json = request.downloadHandler.text;
+                Debug.Log(json);
+            }
+        }
+    }
+}
--- Assets/Scripts/RecordButton.cs
+++ Assets/Scripts/RecordButton.cs
@@ -1,14 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class RecordButton : MonoBehaviour
 {
+    private Button recordButton;
+    private bool buttonPressed;
+    private float startTime;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Button>().onClick.AddListener(Record);
+        recordButton = GetComponent<Button>();
+        recordButton.onClick.AddListener(Record);
+        buttonPressed = false;
     }
 
     // Update is called once per frame
@@ -19,16 +23,33 @@ public class RecordButton : MonoBehaviour
 
     void Record()
     {
+        AudioSource audioSource = GetComponent<AudioSource>();
         if(Microphone.devices.Length <= 0)
         {
             Debug.Log("I don't have microphones to record");
+            return;
+        }
+        if(!buttonPressed)
+        {
+            Debug.Log("I'm recording. I have microphone " + Microphone.devices.Length);
+            audioSource.clip = Microphone.Start(Microphone.devices[0], false, 3599, 44100);
+            buttonPressed = true;
+            startTime = Time.realtimeSinceStartup;
         }
         else
         {
-            Debug.Log("I'm recording.");
-            AudioSource audioSource = GetComponent<AudioSource>();
-            audioSource.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
-            audioSource.Play();
+            Microphone.End(null);
+            float clipLength = Time.realtimeSinceStartup - startTime;
+            int samples = (int)(audioSource.clip.frequency * clipLength);
+            float[] data = new float[samples];
+            audioSource.clip.GetData(data, 0);
+            AudioClip trimmedClip = AudioClip.Create(audioSource.clip.name, samples, audioSource.clip.channels, audioSource.clip.frequency, false);
+            trimmedClip.SetData(data, 0);
+            if(trimmedClip != null)
+            {
+                SavWav.Save("Questions/CurrentQuestion.wav", trimmedClip);
+            }
+            buttonPressed = false;
         }
     }
 }
